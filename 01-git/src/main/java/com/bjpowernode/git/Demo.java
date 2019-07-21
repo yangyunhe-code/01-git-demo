de2ba5351fa5bdc7c7d05d8c643aaf117cf9c120
yun <376157010@qq.com>
2019-07-21 21:59:40 +0800

    first commit

--- 01-git/src/main/java/com/bjpowernode/git/Demo.java
+++ 01-git/src/main/java/com/bjpowernode/git/Demo.java
@@ -7,5 +7,6 @@ package com.bjpowernode.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("欢迎使用git");
+        System.out.println("欢迎使用git");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 01-git-demo 
 欢迎使用git
+欢迎使用git
